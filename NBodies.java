8cb9f493f14cd862f697c7d09046fa305e9038b2
jtchan2 <61221355+jtchan2@users.noreply.github.com>
2020-10-22 19:42:36 -0700

    Add files via upload

--- NBodies.java
+++ NBodies.java
@@ -1,7 +1,17 @@
 import java.util.*;
+
 import javax.swing.JPanel;
+
 import javax.swing.JFrame;
-public class NBodies{
+
+import java.awt.Graphics;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.awt.Color;
+import java.io.IOException;
+import java.io.*;
+
+public class NBodies extends JPanel{
 	private String name;
 	private int mass;
 	private int xCoordinate;
@@ -32,9 +42,28 @@ public class NBodies{
 			return this.name;
 		}
 	}
+
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		g.setColor(Color.RED);
+		g.drawOval(100,100,50,50);
+		g.setColor(Color.YELLOW);
+		g.fillOval(100,200,50,50);
+	}
 	public static void main(String[] args){
 		CelestBody p1= new CelestBody("Earth", 20000, 20, 5,16, 3000, 20000);
 		System.out.println(p1.giveName());
+
+		NBodies t= new NBodies();
+		JFrame jf= new JFrame();
+
+		jf.setTitle("Canvas");
+		jf.setSize(600,400);
+		jf.add(t);
+		jf.setVisible(true);
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
 
 	}
 
