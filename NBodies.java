b833c69907c6e7702531c3db9c7696553de998b2
jtchan2 <61221355+jtchan2@users.noreply.github.com>
2020-11-03 21:14:06 -0800

    Jframe works correctly
    Jframe now doesn't have planets immediately zoom off

--- NBodies.java
+++ NBodies.java
@@ -27,6 +27,7 @@ public class NBodies extends JPanel implements ActionListener{
 	private double scale;
 	private double xforce;
 	private	double yforce;
+	//Gravity is the universal gravity constant
 	private double gravity=6.67*Math.pow(10,-11);
 
 
@@ -40,9 +41,10 @@ public class NBodies extends JPanel implements ActionListener{
 	}
 
 	
-	
-	Timer tm= new Timer(500, this);
+	//timer that is used to update/ aniamte planets, set o 500 because if at 5, planets immidiately dispappear after 1 second where 500 you can see 1 planet for 1 to 2 frames
+	Timer tm= new Timer(50, this);
 
+	//this method bascially initializes the shapes onto the Jframe
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 		g.setColor(Color.RED);
@@ -62,14 +64,17 @@ public class NBodies extends JPanel implements ActionListener{
 		
 		tm.start();
 	}
-
+	//finds the x distance bteween tow planets to scale i.e. distance is multiplied by scale
 	public double findXDistance(CelestBody b1,CelestBody b2){
 		return (b1.getXPos()-b2.getXPos())*scale;
 	}
 
+	//does the same as the FindX but find y distance isntead 
 	public double findYDistance(CelestBody b1,CelestBody b2){
 		return (b1.getYPos()-b2.getYPos())*scale;
 	}
+
+	//find hypothenus distance to be used in Netwen's gravitation equation since that is correct radius distance
 	public double findZDistance(double distance1, double distance2){
 		double zDistance=Math.pow(distance1,2)+Math.pow(distance2,2);
 		zDistance=Math.sqrt(zDistance);
@@ -77,9 +82,12 @@ public class NBodies extends JPanel implements ActionListener{
 
 	}
 
+	//this method basically updates each x and y coordinates of planet on Jframe by doing Newton's Gravity equation
 	public void actionPerformed(ActionEvent e){
-		
+		//for loop used to get gravitation/ force exprienced on each planet to other on the frame
 		for(int i=0; i<eList.size(); i++){
+
+			//this will be holders for velocity after calculated
 			double xVelocityC=0.0;
 			double yVelocityC=0.0;
 			CelestBody planet1= eList.get(i);
@@ -90,8 +98,11 @@ public class NBodies extends JPanel implements ActionListener{
 					double yDist= findYDistance(planet1,planet2);
 					//try using hypothenus instead of xdist and ydist
 					double zDist= findZDistance(xDist,yDist);
-					double xForce= gravity*((planet1.getMass()*planet2.getMass())/Math.pow(zDist,2));
-					double yForce= gravity*((planet1.getMass()*planet2.getMass())/Math.pow(zDist,2));
+
+					//Does the math for x force and y force but will be same since hypotnuses distacne will be used for radius
+					double xForce= gravity*((planet1.getMass()*planet2.getMass())/Math.pow(xDist,2));
+					double yForce= gravity*((planet1.getMass()*planet2.getMass())/Math.pow(yDist,2));
+					//using xDist, yDist or zDist yields same results
 
 					if(planet1.getXPos()-planet2.getXPos()==0){
 						xForce=0.0;
@@ -114,18 +125,27 @@ public class NBodies extends JPanel implements ActionListener{
 
 				}
 			}
+			//planet1.setXVelocity cannot be removed becuase it won't update without it
 			planet1.setXVelocity(planet1.getXVelocity()+(xVelocityC/scale)/planet1.getMass());
+
+			//Same case for setYVelocity
 			planet1.setYVelocity(planet1.getYVelocity()+(yVelocityC/scale)/planet1.getMass());
+
 
 			System.out.println("Origional X coordinate:");
 			System.out.println(planet1.getXPos());
-			planet1.setXCoordinate(planet1.getXPos()+(int)planet1.getXVelocity());
+
+			//the planet1.getXPos()+ can be removed
+			planet1.setXCoordinate((int)planet1.getXVelocity());
 			System.out.println("X place Changes to:");
 			System.out.println(planet1.getXPos());
 
 			System.out.println("Origional Y Coordinate:");
 			System.out.println(planet1.getYPos());
-			planet1.setYCoordinate(planet1.getYPos()+(int)planet1.getYVelocity());
+
+
+			//the planet1.getYPos()+ can be removed
+			planet1.setYCoordinate((int)planet1.getYVelocity());
 			System.out.println("New Y Coordinate:");
 			System.out.println(planet1.getYPos());
 
@@ -150,6 +170,7 @@ public class NBodies extends JPanel implements ActionListener{
 		}
 		System.out.println(storage.get(0)[0]);
 
+		//checks if first line states make LL or Arraylist
 		if(storage.get(0)[0].equals("ArrayList")){
 			System.out.println("Creating ArrayList");
 			eList= new ArrayList<CelestBody>();
@@ -159,9 +180,10 @@ public class NBodies extends JPanel implements ActionListener{
 			eList=new LinkedList1<CelestBody>();
 		}
 
+		//Set constant scale to second item in file
 		scale=Double.parseDouble(storage.get(1)[0]);
 
-
+		//Everythng after first 2 lines are assumed items for a planet thus creating planets
 		for(int i=2; i<storage.size(); i++){
 			name=storage.get(i)[0];
 			mass= Double.parseDouble(storage.get(i)[1]);
